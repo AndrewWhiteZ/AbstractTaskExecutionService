354f4915251e15dc3e580e62ec652f8b90475b17
ZhirnovVA <zhirnov.vladimir@renue.ru>
2024-12-09 21:27:32 +0500

    Инициализированы два сервиса, мастер-сервис, воркер-сервис, а также модуль с библиотеками

---
+++ master-service/src/test/java/ru/raperan/abstracttaskexecutorservice/masterservice/MasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.raperan.abstracttaskexecutorservice.masterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
