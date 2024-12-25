aa5bd5bc31a9d6cbb9cb86682366cc6f1d3b1799
ZhirnovVA <zhirnov.vladimir@renue.ru>
2024-12-25 19:23:22 +0500

    настройка keycloak и авторизация

---
+++ libs/keycloak-security-starter/src/main/java/ru/raperan/abstractTaskExecutorService/KeycloakSecurityAutoConfiguration.java
@@ -0,0 +1,7 @@
+package ru.raperan.abstractTaskExecutorService;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakSecurityAutoConfiguration {
+}
